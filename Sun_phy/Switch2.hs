17076b07e74279f27387b4b0f544d0411fc5c95e
Sébastien Deriaz <71486197+SebastienDeriaz@users.noreply.github.com>
2022-12-05 19:00:54 +0100

    Switch2 source + testbench

--- Sun_phy/Switch2.hs
+++ Sun_phy/Switch2.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ViewPatterns #-}
-module Switch2 where
+module Sun_phy.Switch2 where
 
 import Clash.Prelude
 
@@ -8,7 +8,7 @@ import Clash.Prelude
 -- sel = 1 -> A
 -- sel = 0 -> B
 
-switch
+switch2
     :: forall dom . HiddenClockResetEnable dom
     => Signal dom Bit -- A_valid_i
     -> Signal dom Bit -- A_data_i
@@ -19,10 +19,11 @@ switch
     -> Signal dom Bit -- ready_i
     -> Signal dom Bit -- sel
     -> Signal dom (Bit, Bit, Bit, Bit, Bit) -- A_ready_o B_ready_o valid_o data_o last_o
-switch a_valid_i a_data_i a_last_i b_valid_i b_data_i b_last_i ready_i sel = bundle(a_ready_o, b_ready_o, valid_o, data_o, last_o)
+switch2 a_valid_i a_data_i a_last_i b_valid_i b_data_i b_last_i ready_i sel = bundle(a_ready_o, b_ready_o, valid_o, data_o, last_o)
   where    
-    a_ready_o = mux sel ready_i (pure 0)
-    b_ready_o = mux sel (pure 0) ready_i
-    valid_o = mux sel a_valid_i b_valid_i
-    data_o = mux sel a_data_i b_data_i
-    last_o = mux sel a_last_i b_last_i
+    selb = bitToBool <$> sel
+    a_ready_o = mux selb ready_i (pure 0)
+    b_ready_o = mux selb (pure 0) ready_i
+    valid_o = mux selb a_valid_i b_valid_i
+    data_o = mux selb a_data_i b_data_i
+    last_o = mux selb a_last_i b_last_i
